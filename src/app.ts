61edcc6c79bd2ddccb176efff23e20cd8142b4d9
Chinmay <mailbox.chinmay@gmail.com>
2023-10-05 22:37:19 +0530

    feat: base repo

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import fastify from 'fastify';
+import fastifyPostgres from '@fastify/postgres';
+import fastifyCors from '@fastify/cors';
+import fastifySwagger from '@fastify/swagger';
+import fastifyAutoload from '@fastify/autoload';
+import { dirname, join } from 'path';
+const app = fastify({logger:true});
+
+app.register(fastifyPostgres, {
+    connectionString: 'postgres://postgres:postgres@localhost:5432/postgres'
+});
+
+app.register(fastifyCors, {
+    origin: '*'
+});
+
+app.register(fastifySwagger, {
+    routePrefix: '/docs',
+    swagger: {
+        info: {
+            title: 'Fastify Boilerplate API',
+            version: '1.0.0',
+        },
+    },
+    exposeRoute: true,
+});
+const __dirname = dirname(new URL(import.meta.url).pathname);
+app.register(fastifyAutoload, {
+    dir: join(__dirname, 'routes')
+});
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
+// import { dirname, join } from 'path'
+// import fastify from 'fastify'
+// import autoload from '@fastify/autoload'
+
+
+// const server = fastify()
+
+// server.get('/', async (req, res) => {
+//   return { hello: 'world' }
+// })
+// server.register(autoload, {
+//   dir: path.join(__dirname, 'plugins')
+// })
+// server.listen(3000, (err, address) => {
+//   if (err) {
+//     console.error(err)
+//     process.exit(1)
+//   }
+//   console.log(`Server listening at ${address}`)
+// })
+// fastify full fledeged setup with logger postgres setup for a server
+// Path: src/index.ts
 import fastify from 'fastify'
 
 const server = fastify()
-
-server.get('/', async (req, res) => {
-  return { hello: 'world' }
-})
-
-server.listen(3000, (err, address) => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-  console.log(`Server listening at ${address}`)
-})
