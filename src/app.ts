daed12f720d6df9753fd392ea6481721095a6a6d
Chinmay <mailbox.chinmay@gmail.com>
2023-10-07 20:18:34 +0530

    feat: removed fastify swagger

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import fastify from 'fastify';
 import fastifyPostgres from '@fastify/postgres';
 import fastifyCors from '@fastify/cors';
-import fastifySwagger from '@fastify/swagger';
 import fastifyAutoload from '@fastify/autoload';
 import { dirname, join } from 'path';
 const app = fastify({ logger: true });
@@ -14,16 +13,7 @@ app.register(fastifyCors, {
     origin: '*'
 });
 
-// app.register(fastifySwagger, {
-//     routePrefix: '/docs',
-//     swagger: {
-//         info: {
-//             title: 'Fastify Boilerplate API',
-//             version: '1.0.0',
-//         },
-//     },
-//     exposeRoute: true,
-// });
+
 const __dirname = dirname(new URL(import.meta.url).pathname);
 app.register(fastifyAutoload, {
     dir: join(__dirname, 'routes')
